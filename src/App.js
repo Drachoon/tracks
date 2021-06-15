5490f48c6a8694e14a6a5263b9a00eab7a3727b0
drachoon <drachoon@drachoon-SPN>
2021-06-15 23:07:31 +0300

    Улучшение: Организация папочной структуры.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './css/reset.css';
 import './App.css';
-import Header from'./component/Header';
-import Body from './component/Body';
-import Footer from './component/Footer';
+import Header from'./component/Header/Header';
+import Body from './component/Body/Body';
+import Footer from './component/Footer/Footer';
 
 function App() {
     return (
